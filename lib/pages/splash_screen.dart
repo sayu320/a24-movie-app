be6d1c8c3172dc102e5cdfec881ad24607e42009
sayu320 <sayoojskrishna.sk@gmail.com>
2024-02-22 18:20:22 +0530

    splash screen

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:a24_movie_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => const HomePage())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('lib/assets/a24logo2.jpg'),
+      ),
+    );
+  }
+}
